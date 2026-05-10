3828f6d8fba47ba0881accb11328f663df39a1fa
ratnamukhesh <ratna.mukhesh.j@gmail.com>
2026-05-10 16:01:52 -0700

    Prepared docker image

--- spring-kubernates-ex/build.sh
+++ spring-kubernates-ex/build.sh
@@ -3,7 +3,7 @@ set -e
 
 DOCKER_USERNAME="ratna1987"
 DOCKER_PASSWORD="Docker@1987"
-IMAGE_NAME="ratna/spring-kubernates"
+IMAGE_NAME="ratna1987/spring-kubernates-ex"
 TAG="latest"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
