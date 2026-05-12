4cb55ba0dcb44cb722943c6933779d58140e2e52
ratnamukhesh <ratna.mukhesh.j@gmail.com>
2026-05-11 22:04:32 -0700

    Made some chabges

--- spring-kubernates-ex/build.sh
+++ spring-kubernates-ex/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-DOCKER_USERNAME="ratna1987"
-DOCKER_PASSWORD="Docker@1987"
+
 IMAGE_NAME="ratna1987/spring-kubernates-ex"
 TAG="latest"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t $IMAGE_NAME:$TAG .
+docker build -t $IMAGE_NAME:${BUILD_NUMBER} .
 
-docker push $IMAGE_NAME:$TAG
+docker push $IMAGE_NAME:${BUILD_NUMBER}
